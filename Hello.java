999f66e441e915feac8ed6dc8fa258ff36d57f0f
alanpear <am3485@njit.edu>
2024-02-05 11:34:52 -0500

    adding problem 1

--- Hello.java
+++ Hello.java
@@ -1,9 +1,8 @@
 public class Hello{
     public static void main(String[] args){
-<<<<<<< HEAD
+
         System.out.println("Hello world");       
-=======
-        System.out.println("Hello world");
->>>>>>> 84b0a767b9efbcb70b19bb30ef277d6851fe0d81
+
+        
     }
 }
