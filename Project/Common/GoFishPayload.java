3c38753c2d12b6b65b0f7a4267ffb6fd8f2df764
alanpear <am3485@njit.edu>
2024-03-30 23:35:28 -0400

    stuff

--- Project/Common/GoFishPayload.java
+++ Project/Common/GoFishPayload.java
@@ -1,7 +1,47 @@
 package Project.Common;
 
+import Project.Client.Hand;
+
 public class GoFishPayload extends Payload {
     private String action;
+    private Hand cardList;
+    private String pointList;
+
+
+    public GoFishPayload(String a, Hand c){
+        action = a;
+        cardList = c;
+    }
+    
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getPointList() {
+        return pointList;
+    }
+
+    public void setPointList(String pointList) {
+        this.pointList = pointList;
+    }
+
+    public Hand getCardList() {
+        return cardList;
+    }
+
+    public void setCardList(Hand cardList) {
+        this.cardList = cardList;
+    }
 
     
+
+    @Override
+    public String toString() {
+        return String.format("Action[%s], CardList[%s], Points[%s]", 
+                getAction(), getCardList());
+    }
 }
